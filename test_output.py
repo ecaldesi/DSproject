43cc1ab483a8a2fa25f27813e7ae30f614664882
Anthony Michael Di Falco <adifalco@darrow4.cc.nd.edu>
2017-04-02 16:06:34 -0400

    Haulw

--- test_output.py
+++ test_output.py
@@ -2,57 +2,33 @@
 import string
 
 pageTemplate= '''<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html style="background-color:darkviolet">
+<html style="background-color:lightblue">
 <head>
 <meta content="text/html; charset=ISO-8859-1"http-equiv="content-type">
-<title>Meal Planner App </title>
-<h1 style="color:red">Welcome to our {mealtime} planner application!</h1>
+<title>Book Feed </title>
+<h1 style="color:black">Welcome to your source for information!</h1>
 </head>
-<body style="background-color:darkviolet">
+<body style="background-color:lightblue">
 
-<h2 style="color:blue">Lets get some food!</h2>
-<p style="color:green"> {mealtime_message} </p>
+<h2 style="color:blue" href="https://www.google.com> Trending in Politics ... </h2>
+<p style="color:green"> Find out what's trending YOUR world! </p>
 
-<h2 style="color:blue">Protein </h2>
-<p style="color:green" >Dish: to be spit out from input -- {meat} </p>
-<p style="color:green" >Calories: to be determined </p>
 
-<h2 style="color:blue" >Side 1 </h2>
-<p style="color:green" >Dish: {veggies} </p>
-<p style="color:green" >Calories: to be determined </p>
-
-<h2 style="color:blue" >Side 2 </h2>
-<p style="color:green" >Dish: {fruit} </p>
-<p style="color:green" >Calories: to be determined </p>
-
-<h2 style="color:blue" >Drink </h2>
-<p style="color:green" >Dish: {drink} </p>
-<p style="color:green" >Calories: to be determined </p>
-
-<h2 style="color:blue" >Results </h2>
-<p style="color:green" >Dishes: {meat}, {fruit}, {veggies}, and a {drink} </p>
-<p style="color:green" >Calories: too many </p>
-<link href="https://www.google.com" title="ayy">
-
+<a href="PoliticsSub.html"> /r/politics</a><br>
+<a href="WorldnewsSub.html"> /r/worldnews</a><br>
+<a href="TechnologySub.html"> /r/technology</a><br>
+<a href="FuturologySub.html"> /r/Futurology</a><br>
+<a href="ScienceSub.html"> /r/science</a><br>
+<a href="SportsSub.html"> /r/sports</a><br>
+<a href="BooksSub.html"> /r/Books</a><br>
+<a href="CustomSub.html"> /r/Memes</a><br>
+</form> 
 </body>
 </html>
 '''
    
 def main():
     #VARIABLES TO BE USED IN FUNCTIONS
-    mealtime = raw_input("Enter breakfast, lunch, or dinner: ")
-    if mealtime[0].lower() == 'b':
-        mealtime_message = "Breakfast? Avoid the eggs"
-    if mealtime[0].lower() == 'l':
-        mealtime_message = "Lunch? The salad is pretty popular"
-    if mealtime[0].lower() == 'd':
-        mealtime_message = "Dinner? Good luck"
-    meat = raw_input("Enter your preferred meat, or 0 for meatless: ")
-    ethnic = raw_input("Enter your preferred station, or 0 for any: ")
-    veggies = raw_input("Enter your preffered vegetable, or 0 for any: ") 
-    fruit = raw_input("Enter your preffered fruit, or 0 for any: ")
-    drink = raw_input("Enter a drink if you want to pick your drink (we can match a drink to suite your meal): ")
-
     contents = pageTemplate.format(**locals())
     browseLocal(contents)
  
